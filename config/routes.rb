23d95d12861c2a56cf077b9dce573fd444257461
tajimata <tajimax0107@gmail.com>
2019-08-08 16:14:54 +0900

    chatspace front

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
+  # resources :articles
+  get 'messages' => 'messages#index'
+  root 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
