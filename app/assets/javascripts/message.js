3b5ddc2219b52b66fa954d144198584e02b6b853
tajimata <tajimax0107@gmail.com>
2019-08-15 17:32:34 +0900

    再々提出ぶん

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,58 +1,76 @@
 $(function(){
-  //   $('#new_message').on('submit', function(e){
-  //     e.preventDefault();
-  //     var formData = new FormData(this);
-  //     var url = $(this).attr('action')
-  //     $.ajax({
-  //       url: url,
-  //       type: "POST",
-  //       data: formData,
-  //       dataType: 'json',
-  //       processData: false,
-  //       contentType: false,
-  //     })
-  //   })
-  // });
+
+  // sign = n >= 0 ? 'big' : 'small';
+  // console.log(sign);
+
   function buildHTML(message){
-    if ( message.image ) {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <asset_path src=${message.image} >
-        </div>`
-      return html;
-    } else {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
-  }
+    // if ( message.image ) {
+
+      var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
+      var html = `<div class=message>
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                      ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                      ${message.date}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                      ${message.content}
+                      </p>
+                      ${imagehtml}
+                    </div>
+                  </div> `
+                  return html;
+                }
+
+
+
+  //     var html =
+  //       `<div class="message" data-message-id=${message.id}>
+  //         <div class="upper-message">
+  //           <div class="upper-message__user-name">
+  //             ${message.user_name}
+  //           </div>
+  //           <div class="upper-message__date">
+  //             ${message.date}
+  //           </div>
+  //         </div>
+  //         <div class="lower-message">
+  //           <p class="lower-message__content">
+  //             ${message.content}
+  //           </p>
+  //         </div>
+  //         <asset_path src=${message.image} >
+  //       </div>`
+  //     return html;
+  //   } else {
+  //     var html =
+  //       `<div class="message" data-message-id=${message.id}>
+  //         <div class="upper-message">
+  //           <div class="upper-message__user-name">
+  //             ${message.user_name}
+  //           </div>
+  //           <div class="upper-message__date">
+  //             ${message.date}
+  //           </div>
+  //         </div>
+  //         <div class="lower-message">
+  //           <p class="lower-message__content">
+  //             ${message.content}
+  //           </p>
+  //         </div>
+  //       </div>`
+  //     return html;
+  //   };
+  // }
+
+
+
+
+
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
